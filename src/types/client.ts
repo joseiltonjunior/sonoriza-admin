c906a741cb500c8bc9ffa87f8e1902b2ae50e0d4
joseiltonjunior <joseiltonjuniortech@gmail.com>
2023-07-25 12:19:59 -0300

    feature: add axios, client type, mock api and tests

---
+++ src/types/client.ts
@@ -0,0 +1,15 @@
+export interface ClientProps {
+  id: string
+  name: string
+  cpf: string
+  rg: string
+  issuingBody: string
+  cellPhone: string
+  telephone: string
+  dateOfBirth: string
+  nationality: string
+  stateOfBirth: string
+  cityOfBirth: string
+  maritalStatus: string
+  gender: string
+}
