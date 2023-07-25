31c893514703bf518126ecffd189151eebed18cd
joseiltonjunior <joseiltonjuniortech@gmail.com>
2023-07-25 12:16:12 -0300

    feature: add axios, client type, mock api and tests

---
+++ src/utils/tests/handleFindClientByCpf.ts
@@ -0,0 +1,16 @@
+import request from 'supertest'
+import { ClientProps } from '../../types/client'
+
+export async function handleFindClientByCpf(cpf: string) {
+  const apiUrl = import.meta.env.VITE_API_URL
+
+  const getResponse = await request(apiUrl).get('/clients').send()
+
+  const clients: ClientProps[] = JSON.parse(getResponse.text)
+
+  const clientToEdit = clients.find((client) => client.cpf === cpf)
+
+  if (clientToEdit) {
+    return clientToEdit
+  }
+}
