351767173a9b1acda670bbe76d61f2fe04cfa642
joseiltonjunior <joseiltonjuniortech@gmail.com>
2023-08-03 15:02:44 -0300

    fix: change name, logo and others adjuts

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,4 +1,4 @@
-import logo from '@/assets/logo.svg'
+import logo from '@/assets/logo.png'
 import bell from '@/assets/bell.svg'
 import user from '@/assets/user.svg'
 import { Button } from './Button'
@@ -10,7 +10,7 @@ interface HeaderProps {
 export function Header({ isError }: HeaderProps) {
   return (
     <div className="h-20 flex items-center justify-between bg-white py-5 px-10 shadow-black/10 shadow-xl w-full z-50 fixed">
-      <img src={logo} alt="logo" className="w-32 h-9" />
+      <img src={logo} alt="logo" className="w-32" />
 
       {!isError && (
         <div className="flex gap-6">
