2da4b2b00288819a6215023ab3588fb97046fd24
joseiltonjunior <joseiltonjuniortech@gmail.com>
2023-08-03 15:16:40 -0300

    fix: routes

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,16 @@ import { Hooks } from './hooks/index.tsx'
 import './i18n'
 import './index.css'
 
-import { RouterProvider } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
-import { routes } from './routes/routes.tsx'
+import { App } from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Hooks>
-      <RouterProvider router={routes} />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Hooks>
   </React.StrictMode>,
 )
